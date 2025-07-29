c2d3b6a875f960dcc18ee7a945aa2d2a32938024
Mohammad Davari <modavari005@gmail.com>
2025-07-29 10:12:41 +0330

    database bala omad va bedon moshkel connect shod

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from typing import Annotated, List
+from sqlalchemy import text
+from app.database import get_db, Base, engine
+from app.models import user
 
 app = FastAPI(title="Inventory Management System")
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
@@ -9,3 +17,19 @@ def root():
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+@app.get("Sign Up")
+def sign_up():
+    return {"message": "Sign Up endpoint"}
+
+@app.get("Login")
+def Login():
+    return {"message": "Login endpoint"}
+
+@app.get("/TDB")
+def test_DB(db_session: Session = Depends(get_db)):
+    try:
+        db_session.execute(text("SELECT 1"))
+        return {"message": "Database connection is working"}
+    except Exception as error:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error))
